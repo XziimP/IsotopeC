6e18ef6b3a83cc52e115bacf155df79052a97d95
isotopec-org <isotopec@protonmail.com>
2020-04-29 19:35:17 -0400

    Update protocol.cpp // fix #include and add year and audit

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
+// Copyright (c) 2019-2020 IsotopeC Development Labs
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -9,7 +10,7 @@
 #include "utilstrencodings.h"
 
 #ifndef WIN32
-# include <arpa/inet.h>
+#include <arpa/inet.h>
 #endif
 
 namespace NetMsgType {
