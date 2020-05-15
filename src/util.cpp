bd77ea1e4787c99230243660e46deaa7269ea6cb
isotopec-org <isotopec@protonmail.com>
2020-05-15 00:41:30 -0400

    Update util.cpp // Need to update to <cstdarg> and include boost join.hpp, fstream.hpp and parsers.hpp

--- src/util.cpp
+++ src/util.cpp
@@ -506,7 +506,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "isotopec";
 #endif
     if (pex)
         return strprintf(
